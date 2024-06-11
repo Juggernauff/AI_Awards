0dc6e6a9ca9d70c87e9e3a8f920646eb08026c60
Danila Khodarev <danila.khodarev@simbirsoft.com>
2024-06-11 15:56:17 +0400

    feat: add team info & css styles.

---
+++ AI_Awards/Models/TeamMember.cs
@@ -0,0 +1,18 @@
+﻿namespace AI_Awards.Models
+{
+    public class TeamMember
+    {
+        public string ImagePath { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+
+        public TeamMember() { }
+
+        public TeamMember(string imagePath, string fullName, string role)
+        {
+            ImagePath = imagePath;
+            FullName = fullName;
+            Role = role;
+        }
+    }
+}
